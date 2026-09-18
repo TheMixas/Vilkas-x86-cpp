ca5cdeb5d0b71dbccf8fd6f3392c748e884afe82
Gustas <themixaslt@gmail.com>
2026-09-18 23:48:44 +0100

    x86_64: long mode boot

--- kernel/kernel/kernel.cpp
+++ kernel/kernel/kernel.cpp
@@ -5,4 +5,5 @@ extern "C" void __cxa_pure_virtual() { for (;;) __asm__("hlt"); }
 extern "C" void kernel_main(void) {
 	terminal_initialize();
 	printf("Hello, kernel World in C++!\n");
+	printf("Hello, kernel World in C++!\n");
 }
